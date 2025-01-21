4d6a79276623d694cf8e536fffb1f74ee6d95def
Ram Grover <ramgrover.rg@gmail.com>
2025-01-20 23:10:39 -0500

    Switched to src/index.js as main entry point

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Include browser globals
+        process: "readonly", // Add process as a global for Node.js
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
