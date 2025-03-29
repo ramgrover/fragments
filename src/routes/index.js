695b1f6296d3659f9dbe548e866d77cfe8f8ff94
Ram Grover <ramgrover.rg@gmail.com>
2025-03-29 00:18:19 -0400

    added hostname to health check

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,12 @@
 // src/routes/index.js
-
+const { hostname } = require('os');
 const express = require('express');
-// Our authentication middleware
 const { authenticate } = require('../auth');
 
-// version and author from package.json
 const { version, author } = require('../../package.json');
 const { createSuccessResponse } = require('../response');
-// Create a router that we can use to mount our API
 const router = express.Router();
 
-/**
- * Expose all of our API routes on /v1/* to include an API version.
- */
 router.use(`/v1`, authenticate(),require('./api'));
 router.use((req, res, next) => {
   if (!req.headers['content-type']) {
@@ -20,27 +14,17 @@ router.use((req, res, next) => {
  }
 next();
 });
-/**
- * Define a simple health check route. If the server is running
- * we'll respond with a 200 OK.  If not, the server isn't healthy.
- */
+
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
   res.status(200).json(
     createSuccessResponse({
     status: 'ok',
     author,
-    // Use your own GitHub URL for this!
     githubUrl: 'https://github.com/ramgrover/fragments',
     version,
+    hostname: hostname(),
   }));
 });
-/**
- * Expose all of our API routes on /v1/* to include an API version.
- * Protect them all with middleware so you have to be authenticated
- * in order to access things.
- */
 
 module.exports = router;
