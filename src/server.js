b58bbc2649a2d36ed208b518727410b41d1de4cb
Ram Grover <ramgrover.rg@gmail.com>
2025-04-07 10:11:36 -0400

    Changed the PORT

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = require('./app');
 
 // Get the desired port from the process' environment. Default to `8080`
 //const port = parseInt(process.env.PORT || '8080', 10);
-const port = 8080;
+const port = parseInt(process.env.PORT || '8080', 10);
 
 // Start a server listening on this port
 const server = stoppable(
